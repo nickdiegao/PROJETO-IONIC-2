a81b54e24c9cc7aabbaa00f2a05cce8bb759214a
Nicholas Diego <nickdiegao@gmail.com>
2024-03-21 19:56:48 -0300

    Alteração no footer

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,7 +7,11 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
   alunos = [
-    { nome: 'Nicholas Diego', matricula: '123456' }
+    { nome: 'Nicholas Diego', matricula: '123456' },
+    { nome: 'Josias Nekko', matricula: '789012' },
+    { nome: 'Pedro Henrique', matricula: '345678' },
+    { nome: 'Lucas Gabriel', matricula: '345678' },
+    { nome: 'Christian Lucas', matricula: '345678' }
   ];
 
   constructor() {}
