76453f73da89e6d3187b5925328a294d791495ee
Nicholas Diego <nickdiegao@gmail.com>
2024-03-21 19:49:06 -0300

    Projeto criado Ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  alunos = [
+    { nome: 'Nicholas Diego', matricula: '123456' },
+    { nome: 'Maria', matricula: '789012' },
+    { nome: 'Pedro', matricula: '345678' },
+    { nome: 'Pedro', matricula: '345678' },
+    { nome: 'Pedro', matricula: '345678' },
+    { nome: 'Pedro', matricula: '345678' },
+    { nome: 'Pedro', matricula: '345678' },
+    { nome: 'Pedro', matricula: '345678' }
+  ];
 
   constructor() {}
-
 }
