393f859e40db1761a62e78fdf596c9505bd4ddc6
Nicholas Diego <nickdiegao@gmail.com>
2024-03-21 21:21:54 -0300

    Adicionando nomes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,8 +7,9 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
   alunos = [
-    { nome: 'Nicholas Diego', matricula: '123456' },
-    { nome: 'Josias Nekko', matricula: '789012' },
+    { nome: 'Nicholas Diego', matricula: '01436621' },
+    { nome: 'Ewerton Silva', matricula:'01654963'},
+    { nome: 'Willams Matheus', matricula: '01532905' },
     { nome: 'Pedro Henrique', matricula: '345678' },
     { nome: 'Lucas Gabriel', matricula: '345678' },
     { nome: 'Christian Lucas', matricula: '345678' }
