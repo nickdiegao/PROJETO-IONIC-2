a10cdb89e6680a272f6c9e83a65cf0c04e86748d
Nicholas Diego <nickdiegao@gmail.com>
2024-04-01 22:45:24 -0300

    Inserindo imagens dos alunos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,12 +7,9 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
   alunos = [
-    { nome: 'Nicholas Diego', matricula: '01436621' },
-    { nome: 'Ewerton Silva', matricula:'01654963'},
-    { nome: 'Willams Matheus', matricula: '01532905' },
-    { nome: 'Pedro Henrique', matricula: '345678' },
-    { nome: 'Lucas Gabriel', matricula: '345678' },
-    { nome: 'Christian Lucas', matricula: '345678' }
+    { imagem: 'https://raw.githubusercontent.com/nickdiegao/portfolio/main/assets/NICHOLAS%20PORTFOLIO1.jpg', nome: 'Nicholas Diego', matricula: '01436621' },
+    { imagem: 'https://raw.githubusercontent.com/nickdiegao/portfolio/main/assets/WILL-PORTFOLIO.jpeg', nome: 'Willams Matheus', matricula: '01532905' },
+    { imagem: 'https://raw.githubusercontent.com/nickdiegao/portfolio/main/assets/LUAN-PORTFOLIO.jpeg', nome: 'Luan Guilherme', matricula: '01648011' }
   ];
 
   constructor() {}
